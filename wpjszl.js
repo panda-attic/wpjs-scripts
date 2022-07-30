b5defd3027f08002b15948df2a55c72b78211e4a
iamobj <twboss8@126.com>
2022-07-31 02:59:03 +0800

    调试

--- wpjszl.js
+++ wpjszl.js
@@ -1,5 +1,6 @@
 let body = $request.body
-console.log(`🚀 -> file: wpjszl.js -> line 2 -> body123`, $request)
+console.log(`🚀 -> file: wpjszl.js -> line 2 -> body1234${body}`)
+console.log('object', {a:1});
 // body = JSON.parse(body)
 
 // console.log('body', body)
