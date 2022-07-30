ecd7ece9b099117a36533137e806fa6f694c5675
iamobj <twboss8@126.com>
2022-07-31 02:30:01 +0800

    调试

--- wpjszl.js
+++ wpjszl.js
@@ -1,9 +1,19 @@
-let body = $response.body
-console.log(`🚀 -> file: wpjszl.js -> line 2 -> body`, $response)
-// body = JSON.parse(body)
+// let body = $response.body
+// console.log(`🚀 -> file: wpjszl.js -> line 2 -> body`, $response)
+// // body = JSON.parse(body)
 
-// console.log('body', body)
+// // console.log('body', body)
 
-// body = JSON.stringify(body)
+// // body = JSON.stringify(body)
 
-$done(body)
+// $done(body)
+
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj['result'] = 0;
+body = JSON.stringify(obj);
+
+console.log(body);
+
+$done(body);
